4ab082020d43190a4ff1bc355ac828cf35b7c1f3
Ahmad Studio Dev <ahmadstudiodev@gmail.com>
2018-10-24 12:49:46 +0700

    add pythonbasics to section 4

---
+++ 1. PythonBasics_3List.py
@@ -0,0 +1,52 @@
+#number 1
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [ hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+#number 2
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+#number 3
+#a. A, B, and C
+
+#number 4
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath],
+         ]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
